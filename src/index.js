ddea8f97285be5cb3ae783af4f4fc8284176a922
Alexandr Belchitskiy <oleksandr.belchitskyi@gmail.com>
2022-01-08 13:43:45 +0200

    init: start Reverse Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arrChars = String(n).split("");
+
+    if (arrChars[0] === "-") {
+        arrChars.shift();
+    }
+
+    return arrChars.reverse().join("");
+};
